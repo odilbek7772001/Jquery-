ee222b23a91c8c4111d8a922c3044a491f1172ab
odildev2303 <yaropox776@amankro.com>
2024-05-29 08:32:44 +0500

    jquery project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // hide
+  $('#hide').click(function () {
+    $('p').hide(1000)
+  })
+
+    //   show
+  $('#show').click(function () {
+    $('p').show(1000)
+  })
+});
